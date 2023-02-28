84b482f7adf15ef507970af1d77281e532b0bcba
SAsodekar <Sachinsatish.Asodekar@solera.com>
2023-02-28 12:16:28 +0530

    Add project files.

---
+++ Controllers/MenusController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ExpressoApi.Data;
+
+
+namespace ExpressoApi.Controllers
+{
+    public class MenusController : ApiController
+    {
+        private ExpressoDbContext expressoDbContext = new ExpressoDbContext();
+
+        public IHttpActionResult GetMenus()
+        {
+            var menus = expressoDbContext.Menus.Include("SubMenus");   //Eager Loading -> Loading related entites 
+            return Ok(menus);
+        }
+
+        public IHttpActionResult GetMenu(int id)
+        {
+            
+            var menu = expressoDbContext.Menus.Include("SubMenus").FirstOrDefault(m => m.Id == id);
+
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menu);
+        }
+
+    }
+}
---
+++ Models/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ExpressoApi.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public ICollection<SubMenu> SubMenus { get; set; }   //One to Many (One Menu -> Many SubMenus)
+    }
+}
